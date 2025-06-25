d36f1025ac44e90b26f9adf1397e142a9cff5d70
Savko231333 <erten23233@gmail.com>
2025-06-26 01:30:24 +0300

    Lainch.json and some fixes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ class Program
     {
         StatesReader reader = new StatesReader();
 
-        State StartState = reader.ReadState("States\\Start.xml");
+        State StartState = reader.ReadState("ConsoleApp1\\States\\Start.xml");
 
         StartState.Show();
     }
